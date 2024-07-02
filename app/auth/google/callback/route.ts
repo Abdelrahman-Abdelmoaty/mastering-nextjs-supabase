c2c5f05220a7731a8bbbd45738576087185dab90
Abdelrhman Abdelmoaty <Abdelr7manAbdelmoaty@gmail.com>
2024-07-02 16:26:56 +0300

    updates

--- app/auth/google/callback/route.ts
+++ app/auth/google/callback/route.ts
@@ -12,7 +12,7 @@ export async function GET(request: Request) {
 		const cookieStore = cookies();
 		const supabase = createServerClient(
 			process.env.NEXT_PUBLIC_SUPABASE_URL!,
-			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+			process.env.NEXT_PRIVATE_SUPABASE_ANON_KEY!,
 			{
 				cookies: {
 					get(name: string) {
--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -4,7 +4,7 @@ export function createClient() {
 	// Create a supabase client on the browser with project's credentials
 	return createBrowserClient(
 		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+		process.env.NEXT_PRIVATE_SUPABASE_ANON_KEY!
 	);
 }
 
