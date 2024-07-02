9da18f79236243b3fe7d45b1f92a135d650478db
Abdelrhman Abdelmoaty <Abdelr7manAbdelmoaty@gmail.com>
2024-07-02 16:28:07 +0300

    updates

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -8,7 +8,7 @@ export function createClient() {
 	// which could be used to maintain user's session
 	return createServerClient(
 		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PRIVATE_SUPABASE_ANON_KEY!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 		{
 			cookies: {
 				get(name: string) {
